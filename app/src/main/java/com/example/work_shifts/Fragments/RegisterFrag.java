bf3644893c62281283d16471e83fdecc671c2a77
Yonatan <yonirz111@gmail.com>
2025-01-11 14:10:57 +0200

    Folders and new logo

---
+++ app/src/main/java/com/example/work_shifts/Fragments/RegisterFrag.java
@@ -0,0 +1,11 @@
+package com.example.work_shifts.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.work_shifts.R;
+
+public class RegisterFrag extends Fragment {
+        public RegisterFrag() {
+            super(R.layout.register_frag);
+        }
+}
