25ba1ad5d3316c3c1a12fc7c1135d4e6f75bc404
Mor Moshe <mormoshe0@gmail.com>
2025-01-21 19:30:23 +0200

    added layouts and java

--- app/src/main/java/com/example/work_shifts/Fragments/reportFrag.java
+++ app/src/main/java/com/example/work_shifts/Fragments/reportFrag.java
@@ -17,6 +17,6 @@ public class reportFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.report, container, false);
+        return inflater.inflate(R.layout.add_request, container, false);
     }
 }
