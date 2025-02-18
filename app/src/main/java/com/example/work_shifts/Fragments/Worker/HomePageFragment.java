dda616c1d176058e64f8288b06e3c5f05ff66e6f
Yonatan <yonirz111@gmail.com>
2025-02-18 19:49:58 +0200

    Update HomePageFragment.java

--- app/src/main/java/com/example/work_shifts/Fragments/Worker/HomePageFragment.java
+++ app/src/main/java/com/example/work_shifts/Fragments/Worker/HomePageFragment.java
@@ -30,13 +30,16 @@ public class HomePageFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_page, container, false);
 
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+
+        // Find buttons
         infoBtn = view.findViewById(R.id.btnPersonalInfo);
         paySlipBtn = view.findViewById(R.id.btnPaySlip);
         myShiftBtn = view.findViewById(R.id.myShifts);
         addShiftBtn = view.findViewById(R.id.addShift);
-        removeShiftBtn= view.findViewById(R.id.removeShift);
-        NavController navController = Navigation.findNavController(requireActivity(), R.id.main_nav_graph);
+        removeShiftBtn = view.findViewById(R.id.removeShift);
 
+        // Set click listeners
         infoBtn.setOnClickListener(v -> navController.navigate(R.id.action_homePageFragment_to_personalInfoFrag));
         paySlipBtn.setOnClickListener(v -> navController.navigate(R.id.action_homePageFragment_to_paySlipFrag));
         myShiftBtn.setOnClickListener(v -> navController.navigate(R.id.action_homePageFragment_to_myShiftFrag));
